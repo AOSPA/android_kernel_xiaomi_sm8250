c7d7242c792bd159898ea2216d7b8f98caf79ab7
UP5468-GE-ES44AC <2509942930@qq.com>
2022-11-22 21:51:22 +0800

    input: touchscreen: fts_521: Report fp_state to sysfs
    Change-Id: Id9076085e5fb367f9ff52875f065b0ac31ceeda6

--- drivers/input/touchscreen/fts_521/fts.c
+++ drivers/input/touchscreen/fts_521/fts.c
@@ -4261,6 +4261,9 @@ static void fts_gesture_event_handler(struct fts_ts_info *info,
 
 				if ((info->sensor_sleep && !info->sleep_finger) || !info->sensor_sleep) {
 					info->fod_pressed = true;
+					info->fod_pressed_x = x;
+					info->fod_pressed_y = y;
+					tp_common_notify_fp_state();
 					input_report_key(info->input_dev, BTN_INFO, 1);
 					input_sync(info->input_dev);
 					if (info->fod_id) {
@@ -4309,6 +4312,9 @@ static void fts_gesture_event_handler(struct fts_ts_info *info,
 			info->sleep_finger = 0;
 			info->fod_overlap = 0;
 			info->fod_pressed = false;
+			info->fod_pressed_x = 0;
+			info->fod_pressed_y = 0;
+			tp_common_notify_fp_state();
 			goto gesture_done;
 		}
 #endif
@@ -7511,6 +7517,20 @@ static struct tp_common_ops fod_status_ops = {
 	.show = fod_status_show,
 	.store = fod_status_store,
 };
+
+static ssize_t fp_state_show(struct kobject *kobj,
+                             struct kobj_attribute *attr, char *buf)
+{
+	if (!fts_info)
+		return -EINVAL;
+
+	return sprintf(buf, "%d,%d,%d\n", fts_info->fod_pressed_x, fts_info->fod_pressed_y,
+		       fts_info->fod_pressed);
+}
+
+static struct tp_common_ops fp_state_ops = {
+	.show = fp_state_show,
+};
 #endif
 
 /**
@@ -7761,6 +7781,11 @@ static int fts_probe(struct spi_device *client)
 	ret = tp_common_set_fod_status_ops(&fod_status_ops);
 	if (ret < 0)
 		MI_TOUCH_LOGE(1, "%s %s: Failed to create fod_status node err=%d\n",
+			tag, __func__, ret);
+
+	ret = tp_common_set_fp_state_ops(&fp_state_ops);
+	if (ret < 0)
+		MI_TOUCH_LOGE(1, "%s %s: Failed to create fp_state node err=%d\n",
 			tag, __func__, ret);
 #endif
 #endif
